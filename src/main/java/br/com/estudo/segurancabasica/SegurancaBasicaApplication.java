549a3ca9ee7056369c8b1eb27512e1e94527ffee
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-09-24 17:08:49 -0300

    feat: Configuração inicial com Spring Web e Spring Security

---
+++ src/main/java/br/com/estudo/segurancabasica/SegurancaBasicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.segurancabasica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancaBasicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurancaBasicaApplication.class, args);
+	}
+
+}
